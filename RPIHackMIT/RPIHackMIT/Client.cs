26a432b6c95ff4d8ead8040c5549413c84869205
Jesse Freitas <freitas[dot]jesse[at]gmail.com>
2013-10-05 14:03:53 -0400

    HTTP Client Work

--- RPIHackMIT/RPIHackMIT/Client.cs
+++ RPIHackMIT/RPIHackMIT/Client.cs
@@ -7,5 +7,6 @@ namespace RPIHackMIT
 {
     class Client
     {
+
     }
 }
--- RPIHackMIT/RPIHackMIT/DBRequestHandler.cs
+++ RPIHackMIT/RPIHackMIT/DBRequestHandler.cs
@@ -7,5 +7,11 @@ namespace RPIHackMIT
 {
     class DBRequestHandler
     {
+        public static string app_key = "j8p8yz7est7tuo2";
+        public static string app_secret = "fpbth7k4fft8kcs";
+        public static void get(String URI){
+            RestClient restClient = new RestClient("https://api.dropbox.com");
+        }
+        public static string getAccessCode(String a
     }
 }
