080327deec85ea5ba99f03a04b772dae8e844ff2
Michael Zemsky <zmike808@gmail.com>
2013-10-05 14:03:27 -0400

    oauth test

--- RPIHackMIT/RPIHackMIT/Form1.Designer.cs
+++ RPIHackMIT/RPIHackMIT/Form1.Designer.cs
@@ -28,12 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_LoginEmbed = new System.Windows.Forms.Button();
+            this.brwLogin = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // btn_LoginEmbed
+            // 
+            this.btn_LoginEmbed.Location = new System.Drawing.Point(9, 10);
+            this.btn_LoginEmbed.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_LoginEmbed.Name = "btn_LoginEmbed";
+            this.btn_LoginEmbed.Size = new System.Drawing.Size(92, 50);
+            this.btn_LoginEmbed.TabIndex = 0;
+            this.btn_LoginEmbed.Text = "Login (Embedded Browser)";
+            this.btn_LoginEmbed.UseVisualStyleBackColor = true;
+            this.btn_LoginEmbed.Click += new System.EventHandler(this.btn_LoginEmbed_Click);
+            // 
+            // brwLogin
+            // 
+            this.brwLogin.Location = new System.Drawing.Point(9, 64);
+            this.brwLogin.Margin = new System.Windows.Forms.Padding(2);
+            this.brwLogin.MinimumSize = new System.Drawing.Size(15, 16);
+            this.brwLogin.Name = "brwLogin";
+            this.brwLogin.Size = new System.Drawing.Size(719, 536);
+            this.brwLogin.TabIndex = 1;
+            this.brwLogin.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.brwLogin_DocumentCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(739, 601);
+            this.Controls.Add(this.brwLogin);
+            this.Controls.Add(this.btn_LoginEmbed);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "DropNet.Sample.WinForms";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_LoginEmbed;
+        private System.Windows.Forms.WebBrowser brwLogin;
     }
 }
 
